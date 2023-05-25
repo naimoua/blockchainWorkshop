dfe8df02f07a2a92c78854ca139dfa494c5db94b
NaiMou <houdamouarafmh@gmail.com>
2023-05-25 09:27:34 +0100

    commit-1

---
+++ src/test/java/org/blockchainworkshop/BlockchainWorkshopApplicationTests.java
@@ -0,0 +1,13 @@
+package org.blockchainworkshop;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BlockchainWorkshopApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
---
+++ src/test/java/org/blockchainworkshop/entities/BlockchainTest.java
@@ -0,0 +1,43 @@
+package org.blockchainworkshop.entities;
+
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class BlockchainTest {
+
+    @Test
+    public void mainTest() {
+        // Create a blockchain with difficulty level 2
+        Blockchain blockchain = new Blockchain(2);
+
+        // Create a transaction pool
+        TransactionPool transactionPool = blockchain.getTransactionPool();
+
+        // Create some sample transactions
+        Transaction transaction1 = new Transaction("Alice", "Bob", 10);
+        Transaction transaction2 = new Transaction("Bob", "Charlie", 5);
+        Transaction transaction3 = new Transaction("Charlie", "Alice", 3);
+
+        // Add the transactions to the transaction pool
+        transactionPool.addTransaction(transaction1);
+        transactionPool.addTransaction(transaction2);
+        transactionPool.addTransaction(transaction3);
+
+        // Mine a new block
+        Block block = blockchain.mineBlock();
+
+
+        // Display the mined block
+        System.out.println("Mined Block:");
+        System.out.println(block);
+
+        // Display the updated blockchain
+        System.out.println("Blockchain:");
+        for (Block b : blockchain.getChain()) {
+            System.out.println(b);
+        }
+
+
+    }
+}
